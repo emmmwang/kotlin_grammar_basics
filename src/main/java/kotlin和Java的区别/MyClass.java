f815b19d598b8bbfba5612bb2216d7d829421190
Administrator <954544828@qq.com>
2020-07-14 00:54:02 +0800

    7.属性的set-get方法

---
+++ src/main/java/kotlin和Java的区别/MyClass.java
@@ -0,0 +1,30 @@
+package kotlin和Java的区别;
+
+public class MyClass {
+    static final int SPRING=0;
+    static final int SUMMER=1;
+
+    public static void main(String[] args) {
+        int a=10;
+        double b=20.1;
+        b=a;
+    }
+    public static String getName(){
+        return "西南大学";
+    }
+}
+class person{
+    String name;
+    public void useTool(){
+
+    }
+}
+class animal{
+    String name;
+    public  void sex(){
+
+    }
+}
+
+
+
