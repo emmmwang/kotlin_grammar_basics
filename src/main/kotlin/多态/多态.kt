3cf218804d4597527cfc418b4deaf38e27d92262
Administrator <954544828@qq.com>
2020-07-13 22:42:06 +0800

    6. 多态

---
+++ src/main/kotlin/多态/多态.kt
@@ -0,0 +1,48 @@
+package 多态
+
+open class Animal(){
+    open fun sleep(){
+        println("动物冬眠")
+    }
+}
+
+class Dog(): Animal(){
+    override fun sleep(){
+        println("在家里面冬眠")
+    }
+}
+
+fun whereToSleep(animal: Animal){
+    animal.sleep()
+}
+
+//接口也是多态（大类型
+interface OnClickListener{
+    fun onClick()
+}
+
+class Button:OnClickListener {
+    override fun onClick() {
+        println("按钮被点击了")
+    }
+}
+
+class Image:OnClickListener {
+    override fun onClick() {
+        println("图片被点击了")
+    }
+}
+
+//统一接口
+fun test(listener: OnClickListener){
+    listener.onClick()
+}
+
+fun main() {
+    val an=Animal()
+    var dg=Dog()
+
+    whereToSleep(dg)
+
+    test(Image())
+}
