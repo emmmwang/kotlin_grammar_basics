20a53d90348ccfa59a543477196948f9d4f7f025
Administrator <954544828@qq.com>
2020-07-13 20:12:06 +0800

    1.when语句

---
+++ src/main/kotlin/when语句/when.kt
@@ -0,0 +1,23 @@
+package when语句
+
+fun main() {
+    var num=20
+
+    //更简洁
+    when(num){
+        10->println("10")
+        30-> println("30")
+        40-> println("40")
+        else-> println("其他")
+    }
+
+    //更灵活
+     val result=when(num){
+                in 1..10-> println("z")
+                in 11..59-> println("E")
+                in 60..79-> println("f")
+                else-> println("没有成绩")
+            }
+
+    println("result is $result ")
+}
